002a5f20ced2daf6c897802e40893c65c698c7db
Nikhil Tariyal <nikhil.tariyal@pl.co.za>
2026-01-29 13:05:55 +0530

    Hard & Soft Assertions

--- tests/assertions.spec.js
+++ tests/assertions.spec.js
@@ -2,7 +2,11 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('/index.html');
+  // Hard assertion- Stops the execution
   await expect(page).not.toHaveURL('NIKHIL');
+
+  // Soft assertion- Execution continues but the test mark as failed
+  await expect.soft(page).toHaveURL('TARIYAL');
   await expect(page).toHaveTitle('STORE');
   await expect(page).toHaveURL('https://www.demoblaze.com/index.html');
   await page.getByRole('link',{name:"About us"}).click();
